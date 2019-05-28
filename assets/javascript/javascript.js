50de5e73a9e8bdb907ee33a1c8b578a7dac9a961
gccunanan <gccunanan@gmail.com>
2019-05-27 20:33:11 -0700

    fixing ajax call to return: name, phone#, rating, open/closed

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,31 +4,49 @@
 var token = "Bearer GDx5KhPwYk7gxWz0fa4bK4UbYUtl7lt4HSuk2nomFxp2UJim7d55IXu43LbBGcQcOT6eLF98sZURGQ5qRaQ9Do6ePnnM_lx_4nq3MN7WafdRNmWDmtKRnkSHO83kXHYx";
 var yelp_search_url = "https://api.yelp.com/v3/businesses/search?";
 var cors_anywhere_url = "https://cors-anywhere.herokuapp.com/";
-var address = "portland or";
+var address = "97225";
 var food = "pizza";
 
 var yelp_obj = {
     "url": cors_anywhere_url+yelp_search_url+"location="+address+"&term="+food,
     "method": "GET",
     "headers": {
-      "Authorization": token
+        "Authorization": token
     }
-  }
+}
 
+function get_food(){
   $.ajax(yelp_obj).done(function(yelp_data){
-    console.log(yelp_data);
+    // console.log(yelp_data);
+
+    $('#results-section').empty();
 
     for (var i=0; i<yelp_data.businesses.length;i++){
-        console.log(yelp_data.businesses[i].categories[0].alias)
+        // console.log(yelp_data.businesses[i].name);
+        // console.log(yelp_data.businesses[i].phone);
+        // console.log(yelp_data.businesses[i].price);
+        // console.log(yelp_data.businesses[i].rating);
+        // console.log(yelp_data.businesses[i].is_closed);
+
+    var food_div = $('<div>');
+    food_div.addClass(card);
+    food_div.attr('id', 'food-spot-'+i);
+    $('#food-section').append(food_div)
+
     }
   });
+}
 
 $('#search-button').on('click',function(){
 
     food = $('#food').val().trim();
     address = $('#address').val().trim();
+
     console.log(food);
     console.log(address);
 
+    
+    get_food();
     return false;
+
 })
