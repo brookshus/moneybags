ce78da27035dcd7fc770a3c5044fae4dc7e05bb6
gccunanan <gccunanan@gmail.com>
2019-06-01 20:27:12 -0700

    fixed & completed: Add to list button, clear-eat-list button. completed feature: add & remove from eat-list

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,26 +1,17 @@
-// Search ENDPOINT URL (GET): https://api.yelp.com/v3/businesses/search
-// API KEY = GDx5KhPwYk7gxWz0fa4bK4UbYUtl7lt4HSuk2nomFxp2UJim7d55IXu43LbBGcQcOT6eLF98sZURGQ5qRaQ9Do6ePnnM_lx_4nq3MN7WafdRNmWDmtKRnkSHO83kXHYx
-// var yelp_obj = {
-//     "url": cors_anywhere_url+yelp_search_url+"location="+address+"&term="+food,
-//     "method": "GET",
-//     "headers": {
-//         "Authorization": token
-//     }
-// }
-
+//  =============================== GLOBAL VARIABLES ===============================>>
 var token = "Bearer GDx5KhPwYk7gxWz0fa4bK4UbYUtl7lt4HSuk2nomFxp2UJim7d55IXu43LbBGcQcOT6eLF98sZURGQ5qRaQ9Do6ePnnM_lx_4nq3MN7WafdRNmWDmtKRnkSHO83kXHYx";
 var yelp_search_url = "https://api.yelp.com/v3/businesses/search?";
 var cors_anywhere_url = "https://cors-anywhere.herokuapp.com/";
 var address = "";
 var food = "";
 
-// MY AJAX CALL/FUNCTION =============================================>>
+
+// MY AJAX CALL/FUNCTION ===========================================================>>
 function get_food(queryURL){
-  $.ajax({url: queryURL, method: "GET", headers: {Authorization: token}}).done(function(yelp_data){
-      
+  $.ajax({url: queryURL, method: "GET", headers: {Authorization: token}}).done(function(yelp_data){    
       $('#results-section').empty();
-      
-      console.log(yelp_data);
+      // console.log(yelp_data);
+
     for (var i=0; i<yelp_data.businesses.length;i++){
         // console.log(yelp_data.businesses[i].name);
         // console.log(yelp_data.businesses[i].phone);
@@ -33,7 +24,6 @@ function get_food(queryURL){
     food_div.attr('id', 'food-spot-'+i);
     // food_div.attr('style',"background: url("+yelp_data.businesses[i].image_url+")")
     $('#results-section').append(food_div);
-
     $('#food-spot-'+i).append("<h3>Name: "+yelp_data.businesses[i].name+"</h3>");
     $('#food-spot-'+i).append("<h4>Phone#: "+yelp_data.businesses[i].display_phone+"</h4>");
     $('#food-spot-'+i).append("<h4>Budget ($ - $$$$): "+yelp_data.businesses[i].price+"</h4>");
@@ -44,17 +34,18 @@ function get_food(queryURL){
                                               yelp_data.businesses[i].location.state+" "+
                                               yelp_data.businesses[i].location.zip_code+"</a></h4>");
     $('#food-spot-'+i).append("<h4>Open/Closed: "+yelp_data.businesses[i].is_closed+"</h4>");
-
+    $('#food-spot-'+i).attr("data-name", yelp_data.businesses[i].name);
     var add_button = $('<button>');
     add_button.addClass('btn btn-sm btn-success craving');
     add_button.text('Add To List');
-    add_button.attr('value', yelp_data.businesses[i].name);
+    add_button.attr('data-name', yelp_data.businesses[i].name);
+    add_button.attr('type', 'submit')
     $('#food-spot-'+i).append(add_button);
     }
   });
 };
 
-// MY ON-CLICK FUNCTIONS ===============================================>>
+// MY ON-CLICK FOR MY SEARCH =======================================================>>
 $('#search-button').on('click',function(){
 
     food = $('#food').val().trim();
@@ -70,69 +61,84 @@ $('#search-button').on('click',function(){
 
 });
 
-// CLEAR BUTTON = REMOVES RESULTS FROM #RESULTS-SECTION =================>>
+// CLEAR BUTTON = REMOVES RESULTS FROM #RESULTS-SECTION ============================>>
 $('#clear-button').on('click',function(){
   $('#food').val("");
   $('#address').val("");
   $('#results-section').empty();
 });
 
-// CLEAR #TO-EAT-LIST
+// CLEAR #TO-EAT-LIST & LOCALSTORAGE ===============================================>>
 $('#clear-eat-list').on('click',function(){
   $('#to-eat-list').empty();
+  localStorage.clear();
 });
 
-// MY BITE-LIST (WHAT TO EAT NEXT) ======================================>>
-function renderToEatList(eat_list){
-  $('#to-eat-list').empty();
+// ========================== EAT-LIST FUNCTIONS ===================================>>
 
-  // Giving a value to my listed item ===================================>>
-  for (var i=0;i<eat_list.length;i++){
-    var eat_item = $('<p>');
-    eat_item.text(eat_list[i]);
+// FUNCTION RENDERS MY EAT_LIST ====================================================>>
+function showMeFood(eat_list) {
+  $("#to-eat-list").empty(); // empties out the html
 
-  // Creating a visible button for my list ==============================>>
-    var done_item = $('<button>');
-    done_item.attr("data-to-do", i);
-    done_item.addClass('checkbox btn btn-outline-info btn-sm btn-block');
-    done_item.text("✓");
+  // RENDER MY EAT_LIST
+  for (var i = 0; i < eat_list.length; i++) {
+    var grub_item = $("<p>");
+    // grub_item.text(eat_list[i]); // <<------ (THIS IS AN OPTIONAL PREFERENCE)
 
-  // The text of my item becomes a button ===============================>>
-    eat_item = eat_item.prepend(done_item);
-  
-  // Adding the complete item (as a button) to my list ==================>>
-    $("#to-eat-list").append(eat_item);
+    // THIS CREATES MY LISTED ITEM WITH A ✓
+    var food_done = $("<button>");
+    food_done.attr("data-to-eat", i);
+    food_done.addClass("checkbox");
+    food_done.text("✓"+" "+eat_list[i]);
+
+    // APPENDING BUTTON TO THE <P>
+    grub_item = grub_item.prepend(food_done);
+
+    // ADDING THE BUTTON TO THE TO-EAT-LIST DIV
+    $("#to-eat-list").append(grub_item);
   }
 }
 
-$(document).on('click', '.craving', function(event){
+// ON-CLICK FUNCTION FOR THE BUTTON GENERATED FROM OUR AJAX ALL ====================>>
+$(document).on("click", '.craving', function(event) {
   event.preventDefault();
 
-  var eat_task = $(this).val();
-  
-  eat_list.push(eat_task)
-  // console.log(eat_list);
+  // GET THE DATA-NAME FROM THE BUTTON CLICKED & STORE AS A VARIABLE
+  var eat_task = $(this).data('name');
 
-  renderToEatList(eat_list);
+// ADDING OUR NEW EAT-LIST ITEM TO THE LOCALSTORAGE
+  eat_list.push(eat_task);
+
+// UPDATE MY EAT-LIST 
+  showMeFood(eat_list);
+
+// SAVES MY EAT-LIST TO LOCALSTORAGE / TURN THE LIST FROM AN ARRAY INTO A STRING
+  localStorage.setItem("toeatlist", JSON.stringify(eat_list));
+});
+
+//  ON-CLICK FUNCTION THAT WILL DELETE AN ITEM FROM THE LIST ======================>>
+$(document).on("click", ".checkbox", function() {
+  var to_eat_number = $(this).attr("data-to-eat");
+
+// DELETES THE MARKED ITEM 
+  eat_list.splice(to_eat_number, 1);
+
+// UPDATE MY EAT-LIST 
+  showMeFood(eat_list);
+
+// SAVES MY EAT-LIST TO LOCALSTORAGE / TURN THE LIST FROM AN ARRAY INTO A STRING
   localStorage.setItem('toeatlist', JSON.stringify(eat_list));
 });
 
-$(document).on('click', '.craving', function(){
-  var toDoNumber = $(this).attr('data-to-do');
+// LOADS MY EAT-LIST FROM LOCALSTORAGE ============================================>>
+// JSON.parse TURNS THE ARRAY INTO A STRING
+  var eat_list = JSON.parse(localStorage.getItem('toeatlist'))
 
-  eat_list.splice(toDoNumber, 1);
+// CHECKING IF EAT_LIST EXISTS 
+// IF NOT SET EAT_LIST AS AN EMPTY ARRAY
+  if(!Array.isArray(eat_list)){
+    eat_list =[];
+  }
 
-  // Updates the eat_list
-  renderToEatList(eat_list);
-
-  // Saving the eat list to localstorage & turn the list from an array to a string
-  localStorage.setItem('toeatlist', JSON.stringify(eat_list));
-});
-
-var eat_list = JSON.parse(localStorage.getItem('toeatlist'));
-
-if (!Array.isArray(eat_list)){
-  eat_list = [];
-}
-
-renderToEatList(eat_list);
+// RENDER MY EAT_LIST ON PAGE LOAD 
+  showMeFood(eat_list);
